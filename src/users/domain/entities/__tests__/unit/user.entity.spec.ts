3dcc6036d28bfe915fb19b3ad6ec531b5669af35
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-12-28 17:39:11 -0300

    chore: UserEntity getter methos unit testing

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -13,10 +13,30 @@ describe('UserEntity', () => {
 
     sut = new UserEntity(props);
   });
-  it('Constructor method', () => {
-    expect(sut.props.name).toEqual(props.name);
-    expect(sut.props.email).toEqual(props.email);
-    expect(sut.props.password).toEqual(props.password);
-    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  // it('Constructor method', () => {
+  //   expect(sut.props.name).toEqual(props.name);
+  //   expect(sut.props.email).toEqual(props.email);
+  //   expect(sut.props.password).toEqual(props.password);
+  //   expect(sut.props.createdAt).toBeInstanceOf(Date);
+  // });
+
+  it('Getter of name field', () => {
+    expect(sut.name).toBeDefined();
+    expect(typeof sut.name).toBe('string');
+    expect(sut.name).toEqual(props.name);
+  });
+  it('Getter of email field', () => {
+    expect(sut.email).toBeDefined();
+    expect(typeof sut.email).toBe('string');
+    expect(sut.email).toEqual(props.email);
+  });
+  it('Getter of password field', () => {
+    expect(sut.password).toBeDefined();
+    expect(typeof sut.password).toBe('string');
+    expect(sut.password).toEqual(props.password);
+  });
+  it('Getter of createdAt field', () => {
+    expect(sut.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeInstanceOf(Date);
   });
 });
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -6,7 +6,19 @@ export type UserProps = {
 };
 
 export class UserEntity {
-  constructor(public readonly props: UserProps) {
+  constructor(private readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+  get name() {
+    return this.props.name;
+  }
+  get email() {
+    return this.props.email;
+  }
+  get password() {
+    return this.props.password;
+  }
+  get createdAt() {
+    return this.props.createdAt;
   }
 }
